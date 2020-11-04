4a6bc812aec4a597bff10b47a51a6e1feab19705
Yasitha Gamage <yasithamaxs@gmail.com>
2020-11-04 13:29:36 +0530

    Test Commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>
+          Hello Heading
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
